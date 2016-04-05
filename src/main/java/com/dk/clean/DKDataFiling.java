d96115a1e0447d18f198800ac76e3feddf7d30f6
zhaoxuhao <i2i@foxmail.com>
2016-04-05 10:13:37 +0800

    增加finally，确保最后将临时表删除

--- src/main/java/com/dk/clean/DKDataFiling.java
+++ src/main/java/com/dk/clean/DKDataFiling.java
@@ -10,75 +10,103 @@ public class DKDataFiling {
 	public static void formatRec(String spStr, int fdSum, String srcDirName,
 			String dstDirName, String hostIp, String hostPort, String hostName,
 			String hostPassword) throws Exception {
+
 		String srcTable = "HiveTmpTabl_" + System.currentTimeMillis();
+		String dstTable = srcTable + "_dst";
+
 		Statement stmt = null;
 		String url = "jdbc:hive2://" + hostIp + ":" + hostPort + "/default";
 		stmt = HiveUtil.createStmt(driverName, url, hostName, hostPassword);
 		HiveUtil.createTab(stmt, srcTable, srcDirName);
 
-		String dstTable = srcTable + "_dst";
-		HiveUtil.formatRec(stmt, dstDirName, srcTable, dstTable, spStr, fdSum);
+		try {
+			HiveUtil.formatRec(stmt, dstDirName, srcTable, dstTable, spStr,
+					fdSum);
+			HiveUtil.toExTable(stmt, dstTable);
+		} finally {
 
-		HiveUtil.toExTable(stmt, dstTable);
-		HiveUtil.deleteTab(stmt, srcTable);
-		HiveUtil.deleteTab(stmt, dstTable);
+			HiveUtil.deleteTab(stmt, srcTable);
+			HiveUtil.deleteTab(stmt, dstTable);
+		}
 	}
-	
-	//formatField (String regExStr,int fdNum,String srcDirName,String dstDirName,,String threadNum,String hostIp,String hostName,String hostPassword)
-	public static void formatField(String spStr, int fdSum,String fdNum, String regExStr, String srcDirName,
-			String dstDirName, String hostIp, String hostPort, String hostName,
-			String hostPassword) throws Exception {
+
+	// formatField (String regExStr,int fdNum,String srcDirName,String
+	// dstDirName,,String threadNum,String hostIp,String hostName,String
+	// hostPassword)
+	public static void formatField(String spStr, int fdSum, String fdNum,
+			String regExStr, String srcDirName, String dstDirName,
+			String hostIp, String hostPort, String hostName, String hostPassword)
+			throws Exception {
+
 		String srcTable = "HiveTmpTabl_" + System.currentTimeMillis();
+		String dstTable = srcTable + "_dst";
+
 		Statement stmt = null;
 		String url = "jdbc:hive2://" + hostIp + ":" + hostPort + "/default";
 		stmt = HiveUtil.createStmt(driverName, url, hostName, hostPassword);
 		HiveUtil.createTab(stmt, srcTable, srcDirName);
 
-		String dstTable = srcTable + "_dst";
-		HiveUtil.formatField(stmt, dstDirName, srcTable, dstTable, spStr, fdSum,fdNum,regExStr);
-		
-		HiveUtil.toExTable(stmt, dstTable);
-		HiveUtil.deleteTab(stmt, srcTable);
-		HiveUtil.deleteTab(stmt, dstTable);
+		try {
+			HiveUtil.formatField(stmt, dstDirName, srcTable, dstTable, spStr,
+					fdSum, fdNum, regExStr);
+			HiveUtil.toExTable(stmt, dstTable);
+		} finally {
+			HiveUtil.deleteTab(stmt, srcTable);
+			HiveUtil.deleteTab(stmt, dstTable);
+		}
 	}
 
-	//String selectField (int[] fdAr,String srcDirName,String dstDirName,String threadNum,String hostIp,String hostName,String hostPassword)
-	public static void selectField(String spStr, int fdSum,String fdNum, String srcDirName,
-			String dstDirName, String hostIp, String hostPort, String hostName,
-			String hostPassword) throws Exception {
+	// String selectField (int[] fdAr,String srcDirName,String dstDirName,String
+	// threadNum,String hostIp,String hostName,String hostPassword)
+	public static void selectField(String spStr, int fdSum, String fdNum,
+			String srcDirName, String dstDirName, String hostIp,
+			String hostPort, String hostName, String hostPassword)
+			throws Exception {
 		String srcTable = "HiveTmpTabl_" + System.currentTimeMillis();
+		String dstTable = srcTable + "_dst";
+
 		Statement stmt = null;
 		String url = "jdbc:hive2://" + hostIp + ":" + hostPort + "/default";
 		stmt = HiveUtil.createStmt(driverName, url, hostName, hostPassword);
 		HiveUtil.createTab(stmt, srcTable, fdSum, spStr, srcDirName);
 
-		String dstTable = srcTable + "_dst";
-		HiveUtil.selectField(stmt, dstDirName, srcTable, dstTable, spStr, fdSum,fdNum);
-		
-		HiveUtil.toExTable(stmt, dstTable);
-		HiveUtil.deleteTab(stmt, srcTable);
-		HiveUtil.deleteTab(stmt, dstTable);
+		try {
+			HiveUtil.selectField(stmt, dstDirName, srcTable, dstTable, spStr,
+					fdSum, fdNum);
+
+			HiveUtil.toExTable(stmt, dstTable);
+		} finally {
+			HiveUtil.deleteTab(stmt, srcTable);
+			HiveUtil.deleteTab(stmt, dstTable);
+		}
+
 	}
-	
-	
-	//String selectRec (Stirng whereStr,int fdNum,String srcDirName,String dstDirName,String threadNum,String hostIp,String hostName,String hostPassword)
-	public static void selectRec(String spStr, int fdSum, String whereStr, String srcDirName,
-			String dstDirName, String hostIp, String hostPort, String hostName,
-			String hostPassword) throws Exception {
+
+	// String selectRec (Stirng whereStr,int fdNum,String srcDirName,String
+	// dstDirName,String threadNum,String hostIp,String hostName,String
+	// hostPassword)
+	public static void selectRec(String spStr, int fdSum, String whereStr,
+			String srcDirName, String dstDirName, String hostIp,
+			String hostPort, String hostName, String hostPassword)
+			throws Exception {
 		String srcTable = "HiveTmpTabl_" + System.currentTimeMillis();
+		String dstTable = srcTable + "_dst";
 		Statement stmt = null;
 		String url = "jdbc:hive2://" + hostIp + ":" + hostPort + "/default";
 		stmt = HiveUtil.createStmt(driverName, url, hostName, hostPassword);
 		HiveUtil.createTab(stmt, srcTable, fdSum, spStr, srcDirName);
+		try {
+			HiveUtil.selectRec(stmt, dstDirName, srcTable, dstTable, spStr,
+					fdSum, whereStr);
 
-		String dstTable = srcTable + "_dst";
-		HiveUtil.selectRec(stmt, dstDirName, srcTable, dstTable, spStr, fdSum,whereStr);
-		
-		HiveUtil.toExTable(stmt, dstTable);
-		HiveUtil.deleteTab(stmt, srcTable);
-		HiveUtil.deleteTab(stmt, dstTable);
+			HiveUtil.toExTable(stmt, dstTable);
+		} finally {
+			HiveUtil.deleteTab(stmt, srcTable);
+			HiveUtil.deleteTab(stmt, dstTable);
+		}
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		String hostIp = "192.168.50.102";
 		String hostName = "root";
@@ -87,9 +115,13 @@ public class DKDataFiling {
 		String spStr = ",";
 		String srcDirName = "/zxh/class_build_in";
 		String hostPort = "10000";
-		//formatRec(spStr, fdSum, srcDirName, "/zxh/formatRec", hostIp, hostPort,hostName, hostPassword);
-//		formatField(spStr, fdSum, "1, 2, 3", "0", srcDirName, "/zxh/formatFieldout2", hostIp, hostPort, hostName, hostPassword);
-		selectField(spStr, fdSum, "1,2,3", srcDirName, "/zxh/selectFieldout2", hostIp, hostPort, hostName, hostPassword);
-//		selectRec(spStr, fdSum, "f1 = 1", srcDirName, "/zxh/selectRecout", hostIp, hostPort, hostName, hostPassword);
+		// formatRec(spStr, fdSum, srcDirName, "/zxh/formatRec", hostIp,
+		// hostPort,hostName, hostPassword);
+		// formatField(spStr, fdSum, "1, 2, 3", "0", srcDirName,
+		// "/zxh/formatFieldout2", hostIp, hostPort, hostName, hostPassword);
+		selectField(spStr, fdSum, "1,2,3", srcDirName, "/zxh/selectFieldout2",
+				hostIp, hostPort, hostName, hostPassword);
+		// selectRec(spStr, fdSum, "f1 = 1", srcDirName, "/zxh/selectRecout",
+		// hostIp, hostPort, hostName, hostPassword);
 	}
 }
