02bcd63db4bd103ecd2402042386ae5baa056f3c
zhaoxuhao <i2i@foxmail.com>
2016-04-21 15:22:58 +0800

    增加关闭连接

--- src/main/java/com/dk/statistic/DKStatistic.java
+++ src/main/java/com/dk/statistic/DKStatistic.java
@@ -1,5 +1,7 @@
 package com.dk.statistic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.Statement;
 
 import com.dk.util.HiveUtil;
@@ -20,24 +22,30 @@ public class DKStatistic {
 	 * @param hostName
 	 * @param hostPassword
 	 * @return
+	 * @throws Exception 
 	 */
 	public static long  count(String fun, int fdSum, String spStr, int fdNum,
-			String dirName, String hostIp, String hostPort, String hostName, String hostPassword) {
+			String dirName, String hostIp, String hostPort, String hostName, String hostPassword) throws Exception {
 		
 		// 临时表名
 		String tableName = "HiveTmpTabl_" + System.currentTimeMillis();
-		Statement stmt = null;
+		
 		String url = "jdbc:hive2://" + hostIp + ":"+hostPort+"/default";
+		Class.forName(driverName);
+		Connection conn = DriverManager.getConnection(url, hostName,hostPassword);
+		Statement stmt = conn.createStatement();
 		long data = 0;
 		
 		try {
 			stmt = HiveUtil.createStmt(driverName, url, hostName, hostPassword);
 			HiveUtil.createTab(stmt, tableName, fdSum, spStr, dirName);
 			data = HiveUtil.selectTab(stmt, tableName, fun, fdNum);
-			HiveUtil.deleteTab(stmt, tableName);
+			
 
-		} catch (Exception e) {
-			e.printStackTrace();
+		} finally{
+			HiveUtil.deleteTab(stmt, tableName);
+			stmt.close();
+			conn.close();
 		}
 
 		//return "the " + fun + " for field_" + fdNum + " is " + data;
@@ -58,25 +66,30 @@ public class DKStatistic {
  * @param hostName
  * @param hostPassword
  * @return
+ * @throws Exception 
  */
 	//count( fun,  fdSum,  spStr,  fdNum, compStr, whereStr,	 dirName,  hostIp,  hostName,  hostPassword)
 	public static long count(String fun, int fdSum, String spStr, int fdNum,
 			String compStr, String whereStr, String dirName, String hostIp, String hostPort,
-			String hostName, String hostPassword) {
+			String hostName, String hostPassword) throws Exception {
 		// 临时表名
 				String tableName = "HiveTmpTabl_" + System.currentTimeMillis();
-				Statement stmt = null;
+				
 				String url = "jdbc:hive2://" + hostIp + ":"+hostPort+"/default";
+				Class.forName(driverName);
+				Connection conn = DriverManager.getConnection(url, hostName,hostPassword);
+				Statement stmt = conn.createStatement();
 				long data = 0;
 				
 				try {
 					stmt = HiveUtil.createStmt(driverName, url, hostName, hostPassword);
 					HiveUtil.createTab(stmt, tableName, fdSum, spStr, dirName);
 					data = HiveUtil.selectTab(stmt, tableName, fun, fdNum,compStr,whereStr);
+					
+				} finally{
 					HiveUtil.deleteTab(stmt, tableName);
-
-				} catch (Exception e) {
-					e.printStackTrace();
+					stmt.close();
+					conn.close();
 				}
 
 				//return "the " + fun + " for field_" + fdNum + " is " + data;
