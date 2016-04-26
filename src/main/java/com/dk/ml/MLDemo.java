bb3e4bd26fb3096d2d51d1aff19ec09d42e717f9
zzy <chaoren399@126.com>
2016-04-26 16:18:52 +0800

    no message

--- src/main/java/com/dk/ml/MLDemo.java
+++ src/main/java/com/dk/ml/MLDemo.java
@@ -4,4 +4,13 @@ package com.dk.ml;
  * Created by zzy on 16/4/26.
  */
 public class MLDemo {
+    public static void main(String[] args) {
+        DKML dkml = new DKML();
+        // 1. 上传 jar 包
+//        dkml.upLoadJar();
+        // 2. 训练模型
+//        dkml.SVMModelBuild();
+        // 3. 预测
+//        dkml.SVMModelPredict();
+    }
 }
